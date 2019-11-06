9f09f39f8590cf818c3760c850d3aea12caea9b8
shangjinlong <shangjinlong@unisound.com>
2019-11-06 15:27:17 +0800

    Integrate stringhash

---
+++ string_hash/inc/uni_stringhash.h
@@ -0,0 +1,37 @@
+/**************************************************************************
+ * Copyright (C) 2019-2020  junlon2006
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **************************************************************************
+ *
+ * Description : uni_stringhash.h
+ * Author      : junlon2006@163.com
+ * Date        : 2019.11.06
+ *
+ **************************************************************************/
+#ifndef STRING_HASH_INC_UNI_STRINGHASH_H_
+#define STRING_HASH_INC_UNI_STRINGHASH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned int GetStringHashCode(const char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // STRING_HASH_INC_UNI_STRINGHASH_H_
