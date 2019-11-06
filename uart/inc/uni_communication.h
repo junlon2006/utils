2dd664f64e67cf95e7e2ecae323308eaf9d5dbde
shangjinlong <shangjinlong@unisound.com>
2019-11-06 13:52:58 +0800

    Format code

--- uart/inc/uni_communication.h
+++ uart/inc/uni_communication.h
@@ -1,5 +1,29 @@
-#ifndef _UTILS_UNI_COMMUNICATION_H_
-#define _UTILS_UNI_COMMUNICATION_H_
+/**********************************************************************
+ * Copyright (C) 2017-2017  junlon2006
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **********************************************************************
+ *
+ * Description : uni_communication.h
+ * Author      : junlon2006@163.com
+ * Date        : 2017.9.19
+ *
+ **********************************************************************/
+
+#ifndef UART_INC_UNI_COMMUNICATION_H_
+#define UART_INC_UNI_COMMUNICATION_H_
 
 #ifdef __cplusplus
 extern "C" {
